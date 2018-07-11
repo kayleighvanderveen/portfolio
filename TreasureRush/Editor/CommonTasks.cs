3400a8b842c228c926ed8da74e36944817d2e66f
kayleighvanderveen <39193724+kayleighvanderveen@users.noreply.github.com>
2018-07-11 10:58:53 +0200

    Add files via upload

---
+++ TreasureRush/Editor/CommonTasks.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace TreasureRush {
+
+	public class CommonTasks {
+
+		[MenuItem("Treasure Rush/Start Game")]
+		public static void PlayIntroScene() {
+			string introScenePath = EditorBuildSettings.scenes[0].path;
+			EditorApplication.SaveCurrentSceneIfUserWantsTo();
+			EditorApplication.OpenScene(introScenePath);
+			EditorApplication.isPlaying = true;
+		}
+	}
+}
